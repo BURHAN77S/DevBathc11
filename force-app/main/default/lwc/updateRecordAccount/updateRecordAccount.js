f2dea931a08effa34087c430ea030bbde2925695
Burhan Celik <burhanceliks77@gmail.com>
2023-03-07 15:06:02 -0500

    3-7-2023 update

--- force-app/main/default/lwc/updateRecordAccount/updateRecordAccount.js
+++ force-app/main/default/lwc/updateRecordAccount/updateRecordAccount.js
@@ -1,44 +1,80 @@
-import { LightningElement, wire } from 'lwc';
-import NAME_FIELD from '@salesforce/schema/Account.Name';
-import TYPE_FIELD from '@salesforce/schema/Account.Type';
-import INDUSTRY_FIELD from '@salesforce/schema/Account.Industry';
-import REVENUE_FIELD from '@salesforce/schema/Account.AnnualRevenue';
+
+import { LightningElement, track, wire } from 'lwc';
+
+import {ShowToastEvent} from 'lightning/platformShowToastEvent';
+import NAME from '@salesforce/schema/Account.Name';
+import TYPE from '@salesforce/schema/Account.Type';
+import INDUSTRY from '@salesforce/schema/Account.Industry';
+import ANNUALREVENUE from '@salesforce/schema/Account.AnnualRevenue';
 import ACCOUNT_OBJECT from '@salesforce/schema/Account';
 import { getObjectInfo, getPicklistValuesByRecordType } from 'lightning/uiObjectInfoApi';
+import { getRecord, updateRecord } from 'lightning/uiRecordApi';
+
+
+const FIELDS=[NAME,TYPE,INDUSTRY,ANNUALREVENUE];
 
 export default class UpdateRecordAccount extends LightningElement {
-    recordId ='001Do000008kt5OIAQ';
-    typeOptions =[];
-    industryOptions = [];
 
-    @wire (getObjectInfo, {objectApiName : ACCOUNT_OBJECT})
-    accInfo;
+    recordId="001Do000008kt5OIAQ";
+    typeOptions=[];
+    industryOptions=[];
+    @track formData={}
+   /**
+    * 1.fetch the record from database - get record
+    * 2.fetch the comcocox options from metadata -
+    *   a-getObjectInfo 
+    *   b-getpicklistvalueByRecordType (iki tane picklist oldugu icin)
+    *3.update record
+    */
+    @wire (getObjectInfo,{objectApiName: ACCOUNT_OBJECT})
+    accInfo; //sistem tum data bilgilerini bu local property icine geciyor.
+    //accInfo.data.fields seklinde yazip ulasabiliyoruz.burada console.log yapma sansimiz yok
+    //accInfo.data.defaultRecordTypeId seklinde de yazabiliyoruz.
 
-    @wire (getPicklistValuesByRecordType, {objectApiName:ACCOUNT_OBJECT, recordTypeId:'$accInfo.data.defaultrecordTypeId'})
-    picklistHandler({data, error}){
+    @wire(getPicklistValuesByRecordType,{objectApiName:ACCOUNT_OBJECT,recordTypeId:'$accInfo.data.defaultRecordTypeId'})
+    picklistHandler({data,error}){
         if(data){
-            this.typeOptions = data.picklistValues.Type.vules;
-            this.industryOptions = data.picklistValues.Industry.vules;
+        this.typeOptions=data.picklistFieldValues.Type.values;
+        this.industryOptions=data.picklistFieldValues.Industry.values;
         }
         if(error){
             console.error(error);
         }
     }
-    //accInfo.data.defaultrecordTypeId
-    
-    /**
-     * 1. Fetch the record from database - getRecord
-     * 2. Fetch the combobox options from medadata - 
-     *    a. getObjectInfo
-     *    b. getPicklistValuesByRecordType
-     * 3. Update the record.
-     */
+    @wire(getRecord,{recordId:'$recordId', fields:FIELDS})
+    account;
+
+
+
 
     changeHandler(event){
+        //const name = event.target.name;
+        //const value=event.target.value;
 
+        const {name,value} =event.target;
+
+        this.formData[name]=value;
     }
-
     saveAccount(){
+        this.formData["Id"]=this.recordId //hardcording //recordu degistirebilmek icin once id ye ihtiyacimiz var. 
+        console.log('Form Data==> '+ JSON.stringify(this.formData));
+        const recordinput={
+            fields:this.formData
+        };
+
+    updateRecord(recordinput)
+        .then(result=>{
+            console.log("Result",result);
+                const toast= new ShowToastEvent({
+                    title:"Success",
+                    message:"Record has been updated successfully",
+                    variant:"success"
+                });
+                 this.dispatchEvent(toast);
+        })
+        .catch(error=>{
+            console.error(error);
+        })
 
     }
 }
---
+++ force-app/main/default/lwc/wiredApex/wiredApex.js
@@ -0,0 +1,15 @@
+import { LightningElement, wire } from 'lwc';
+import getTopAccounts from '@salesforce/apex/AccountCtrl.getTopAccounts';
+
+const COLUMNS = [{ label: 'Account Name', fieldName: 'Name', type:"text" },
+{ label: 'Type', fieldName: 'Type', type: 'text' },
+{ label: 'Industry', fieldName: 'Industry', type: 'text' },
+{ label: 'Annual Revenue', fieldName: 'AnnualRevenue', type: 'currency' }
+];
+
+export default class WiredApex extends LightningElement {
+    columns = COLUMNS;
+
+    @wire (getTopAccounts)
+    accounts;
+}
