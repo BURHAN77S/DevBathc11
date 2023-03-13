33c47d1710f5b869ad8358b28d10eb9ef4a1a565
Burhan Celik <burhanceliks77@gmail.com>
2023-03-13 14:43:46 -0400

    3-13-2023 update

---
+++ force-app/main/default/lwc/p2cNonPrimitivesParent/p2cNonPrimitivesParent.js
@@ -0,0 +1,9 @@
+import { LightningElement } from 'lwc';
+
+export default class P2cNonPrimitiveParent extends LightningElement {
+    location = {
+        state : "Ohio",
+        city : "Loveland",
+        street : "245 South Second Street Apt 8"
+    };
+}
